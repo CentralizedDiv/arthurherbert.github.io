1a654342bc3113f2585b3f61ec731034b3c23815
sortuzin <arthurhsmelo@gmail.com>
2017-05-27 03:07:30 -0300

    Controle Contato
    ControleContato.js adicionado, criado os métodos pra popular as tabelas,
    corrigido a forma que os daods eram trazidos do banco(agr tem
    json_encode) e mais coisa pra carai

--- php/contato.php
+++ php/contato.php
@@ -1,22 +1,38 @@
-<?php
- 
-    require_once("contatoTicket.php");
-    $ticket = new ContatoTicket();
-
+<?php 
+require_once("contatoTicket.php");
+$ticket = new ContatoTicket();
+$contato = new Contato($ticket);
+//Verify if ajax pass the method and choose what parameter pass to the query
+if(isset($_POST['method']) && !empty($_POST['method'])) {
+    $method = $_POST['method'];
+    switch($method) {
+        case 'fornecedor' : $contato->getContatoFornecedor();break;
+        case 'trabalho'   : $contato->getContatoTrabalho();break;
+        case 'sugrec'     : $contato->getContatoSugRec();break;
+        case 'outros'     : $contato->getContatoOutros();break;
+    }
+}
+class Contato{
+    protected $ticket;
+    function __construct($ticket){
+        $this->ticket = $ticket;
+    }
     function getContatoFornecedor() { 
-        return $ticket->getData('1');
+        return $this->ticket->getDataTable('1');
     }
 
     function getContatoTrabalho() {
-        return $ticket->getData('2');
+        return $this->ticket->getDataTable('2');
     }
 
-    function getContatoSugRe() {
-        return $ticket->getData('3');
+    function getContatoSugRec() {
+        return $this->ticket->getDataTable('3');
     }
 
     function getContatoOutros() {
-        return $ticket->getData('4');
+        return $this->ticket->getDataTable('4');
     }
 }
+
+
 ?>
