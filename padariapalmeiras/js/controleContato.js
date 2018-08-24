9c9701c6b47f03ab0a87ed8352c061c2da460eab
Arthur Melo <arthur.melo@teknisa.com>
2018-08-24 09:22:26 -0300

    Changing rep structure

---
+++ padariapalmeiras/js/controleContato.js
@@ -0,0 +1,297 @@
+$(document).ready(function(){
+	if(location.pathname.split('/').slice(-1)[0] === 'login.html'){
+		$('input').focus(function(){
+			var label = $(this).prev()	
+			label.stop().css({
+				"font-size":"25px",
+				"margin-bottom":"5px",
+				"transform": "scale(0.7)",
+			});
+		});	
+		$('input').focusout(function(){
+			var label = $(this).prev()	
+			if((this).value === ""){
+				label.stop().css({
+					"font-size":"30px",
+					"margin-bottom":"-35px",
+					"transform": "scale(1)",
+					});
+			}
+		});	
+	}
+
+/*Function to call ajax for contato.html's form*/
+	if(location.pathname.split('/').slice(-1)[0] === 'contato.html'){
+		$(".form-horizontal").find("input,textarea,select").jqBootstrapValidation(
+		    {
+			    preventSubmit: true,
+			    submitError: function($form, event, errors) {
+			    },
+			    submitSuccess: function($form, event) {
+			        event.preventDefault(); 
+			        var formContato = {};
+	                formContato.name = $("input#name").val();
+	                formContato.email = $("input#email").val();
+	                formContato.phone = $("input#phone").val();
+	                formContato.assunto = $("select#assunto").val();
+	                formContato.mensagem = $("textarea#mensagem").val();
+	                var jsonContato = JSON.stringify(formContato);               
+	                $.ajax({
+	                    url: "php/contato.php",
+	                    type: "POST",
+	                    data: {dados: jsonContato},
+	                    cache: false,
+	                    success: function(response){
+	                    	if(response === 'true'){
+	                    		alert('Mensagem Enviada, aguarde sua resposta em seu email');
+	                    		$('.form-horizontal').find("input, textarea").val("");
+	                    		$('.form-horizontal').find("select").val("Quero ser um fornecedor");
+							}
+	                    },
+	                    error: function(){
+	                    	alert('Erro ao enviar a mensagem. Que tal ligar pra gente? (31) 3511-7175 ou (31) 3162-0030')
+	                    }
+	                });
+			    },
+			    filter: function() {
+			        return $(this).is(":visible");
+			    }
+			}
+		);
+	}
+
+/*Funtions do call each 'getDataSet*/
+	$("#tabFor").on('click touch', function(){
+		getDataSet('fornecedor');
+	});
+	$('#tabTra').on('click touch', function(){
+		getDataSet('trabalheConosco');
+	});
+	$('#tabSug').on('click touch', function(){
+		getDataSet('sugRec');
+	});
+	$('#tabOut').on('click touch', function(){
+		getDataSet('outros');
+	});
+
+/**
+  *
+  * Function to call ajax to get the dataSet from the tabbed passed as parameter
+  *	@param {String}: name of the tabbed that have the table to be populated
+  *
+ **/
+	function getDataSet(tab){
+		var tab = tab;
+		$.ajax({
+		   url:   'php/contato.php',
+		   type:  'POST',
+		   data:  {method: tab},
+		   error: function() {
+		         alert('Erro ao tentar ação!');
+		   },
+		   success: function(dataSet){ //Performs an async AJAX request
+	       		populateAndUpdateTables(dataSet, tab);
+	        }
+		});
+	 };
+
+ /**
+  *
+  * Function to populate the tables of the tabbed passed as parameter
+  *	@param {Array}: dataSet returned from backend with the contacts, 
+  * {String}: name of the tabbed that have the table to be populated
+  *
+ **/
+	function populateAndUpdateTables(dataSet, tab){
+		var tab   = tab;
+		var table = $('#'+ tab);
+		var rows  = $('#'+ tab).find('tbody > tr');
+		var headers = table.find('thead').find('th');
+		headers = $.makeArray(headers);
+		if($.isArray(dataSet)){
+			//Get the rows of the current table and delete them to update
+			var rows = $('#'+ tab).find('tbody > tr');
+			rows.each(function(){
+				$(this).remove();
+			})
+			//Sort the DataSet
+			dataSet = sortDataSetAccToHeaders(dataSet, headers);
+			dataSet.forEach(function(row){
+				//Get the rows of the dataSet and populate the table with them
+				table.find('tbody').append($('<tr>').attr('id', 'currentRow').attr('name', 'row'));
+				//Build each row of the table, fields that have headers in html as visible and the rest as invisible
+				$.each(row, function(field, value){
+					var isVisible = false;
+					headers.forEach(function(header){
+						if(header.id === field){
+							$('#currentRow').append($('<td>').text(value).css('display', 'table-cell').attr('id', field));
+							isVisible = true;
+						}
+					});	
+					if(!isVisible){
+						$('#currentRow').append($('<td>').text(value).css('display', 'none').attr('id', field));	
+					}			
+				});
+				$('#currentRow').removeAttr('id');		
+			})	
+		}
+	};
+
+/**
+  *
+  * Function to sort the dataSet according to the headers passed as
+  * parameter, this headers are the 'th' tags from controleContato.html
+  *	@param {Array}: dataSet returned from backend with the contacts, 
+  * {Array}: array of headers: 'th' tags from html
+  *
+  * @return {Array}: dataSet sorted according to headers
+  *
+ **/
+	function sortDataSetAccToHeaders(dataSet, headers){
+		var sortedDataSet = [];
+		dataSet.forEach(function(row){
+			var sortedRow = {};
+			headers.forEach(function(header, index){
+				$.each(row, function(field, value){
+					if(field === header.id){
+						sortedRow[field] = value;
+					}
+					if(index === (headers.length -1)){
+						sortedRow[field] = value;	
+					}
+				});
+			});
+			sortedDataSet.push(sortedRow);			
+		});
+		return sortedDataSet;
+	};   
+
+	if(location.pathname.split('/').slice(-1)[0] === 'controleContato.html' || location.pathname.split('/').slice(-1)[0] === 'controleContato.php'){
+		/*Start with the active table populated*/
+		var activeTabbed = ($.makeArray($('.tab-content > .active')))[0];
+		if(activeTabbed)
+			getDataSet(activeTabbed.id);
+		$(document).on('click touch', 'tbody > tr', function(){
+				var currentRow = $.makeArray($(this).find('td'));
+				var positionPointer   = $(this).offset();
+				openSwipe(currentRow, positionPointer);
+		});
+		$('#swipe > a').on('click touch', function(){
+				var swipe = $('#swipe');
+				var positionPointer   = $(this).offset();
+				closeSwipe(swipe, positionPointer);
+		});
+	}
+
+	function openSwipe(row, positionPointer){
+		var swipe = $('#swipe');
+		var swipeAction = $('#swipe > a');
+		var swipePointer = $('#swipe > .swipe-pointer');
+		var positionPointer = positionPointer.top-15;
+		//mobile
+		if(jQuery(window).width() < 786){
+			swipe.stop().css({
+				"margin-left":"0",
+				width:"100%",
+				heigth:"100%",
+				display:"",
+				top:0
+			});
+			swipeAction.stop().css({
+				"margin-left":"0",
+				"margin-top":"2000px"
+			});
+			swipe.removeClass('closed');	
+			swipe.addClass('open');
+			setVisibilityActions();
+		}
+		//desktop
+		else{
+			swipe.stop().css({
+				"margin-left":"29.9%",
+				heigth:"100%",
+				display:"",
+				top:0
+			});
+			swipeAction.stop().css({
+				"margin-left":"0",
+			});
+			swipePointer.stop().css({
+				"top":positionPointer	
+			});
+			swipe.removeClass('closed');	
+			swipe.addClass('open');
+			setVisibilityActions();
+		}
+		populateSwipe(row);
+	};
+
+	function closeSwipe(swipe){
+		var swipePointer = $('#swipe > .swipe-pointer');
+		swipe.stop().css({
+			"margin-left":"100%",
+			width:"70%",
+			top:0
+		});
+		swipePointer.stop().css({
+			"top":"-5000px"	
+		});
+		swipe.removeClass('open');	
+		swipe.addClass('closed');
+		setVisibilityActions();
+	};
+
+	function setVisibilityActions(){
+		var containerAction = $('a.container-action');
+		var swipe = $('#swipe');
+		if(swipe.hasClass("open")){
+			containerAction.stop().css({
+				"margin-left":"40%"
+			});
+		}else if(swipe.hasClass("closed")){
+			containerAction.stop().css({
+				"margin-left":"-90%"
+				
+			});
+		}
+	};
+
+	function populateSwipe(row){
+		var swipe        = $('#swipe');
+		//filds Header and Footer
+		var fieldsSHAndF = $.makeArray(swipe.find('.panel > .panel-heading > label, .panel > .panel-footer > label'));
+		//filds Body
+		var fieldsSB     = $.makeArray(swipe.find('.control-group > .controls > label'));
+		
+		fieldsSHAndF.forEach(function fillSwipesHeaderAndFooter(field){
+			row.forEach(function loopThroughCurrentRow(value){
+				if(field.attributes.name.value === value.id){
+					var valueField = value.textContent;
+					var label = field.textContent.split(":", 1)[0]
+					field.textContent = label+': '+valueField;
+				}
+
+			});
+		});
+
+		fieldsSB.forEach(function fillSwipesBody(field){
+			row.forEach(function loopThroughCurrentRow(value){
+				if(field.attributes.name.value === value.id){
+					var type = field.attributes.type.value;
+					var valueField = value.textContent;
+					if(!document.getElementById('field'+value.id)){
+						$('<'+type+'></'+type+'>').val(valueField).insertAfter('.controls > label[name='+field.attributes.name.value+']').attr('id', 'currentField').attr('class', 'form-control');
+						if($('#currentField').val() !== "")
+							$('#currentField').prop('readonly', true)	
+					}else{
+						$('#field'+value.id).val(valueField).attr('id', 'currentField');;
+						if($('#currentField').val() !== "")
+							$('#currentField').prop('readonly', true)	
+					}
+					$('#currentField').attr('id', 'field'+value.id);	
+				}
+
+			});
+		});
+	};
+});
---
+++ padariapalmeiras/php/contatoTicket.php
@@ -0,0 +1,172 @@
+<?php
+class ContatoTicket{
+
+/**
+  *
+  * Function to get the contacts from database 
+  *	param {String}: code to filter the query by type of contact
+  *
+ **/
+	public function getDataTable($assunto) {
+		//LINK QUEBRADASSO
+		//$link = new mysqli('mysql.hostinger.com.br', 'u832569433_arpe', 'SekA9KkSrt7U', 'u832569433_padar');
+
+		$link = new mysqli('localhost', 'root', '', 'padaria');
+		$query = "SELECT * FROM `contatos` WHERE `CDTIPCON` = '$assunto' ORDER BY `NRSEQUE` DESC";
+		$resulBanco = mysqli_query($link, $query);
+		$result = [];
+		if($resulBanco) {
+		    while ($row = mysqli_fetch_array($resulBanco)) { // fetches a result row as an  array
+		        //create an Object with each row returned
+		        $object = new stdClass();
+		        $object->ASSUNTO  = $row['ASSUNTO'];
+		        $object->EMAIL 	  = $row['EMAIL'];
+		        $object->DTCONTA  = $row['DTCONTA'] ;
+		        $object->NRSEQCON = $row['NRSEQCON'];
+		        $object->NRSEQUE  = $row['NRSEQUE']; 
+		        $object->NOMECLIE = $row['NOMECLIE'];
+		        $object->MENSAGEM = $row['MENSAGEM'];
+		        $object->CDTIPCON = $row['CDTIPCON'];
+		        $object->TELEFONE = $row['TELEFONE'];
+		        $object->DTRESPO  = $row['DTRESPO'] ;
+		        $object->CDOPERA  = $row['CDOPERA'] ;
+		        $object->RESPOSTA  = $row['RESPOSTA'] ;
+		        $result[] = $object;
+		    }
+		}
+    	mysqli_close($link); // close connection with database
+    	header('Content-Type: application/json');
+    	return $result;
+	}	
+
+	public function saveContato($row) {
+		// Prepara a row para inserção no banco de dados
+		switch ($row['ASSUNTO']) {
+			case 'Quero ser um fornecedor':
+				$row['ASSUNTO']  = 'FORNCEDOR';
+				$row['CDTIPCON'] = 1;
+				break;
+			case 'Trabalhe Conosco':
+				$row['ASSUNTO'] = 'TRABALHE';
+				$row['CDTIPCON'] = 2;
+				break;
+			case 'Sugestão/Reclamação':
+				$row['ASSUNTO'] = 'SUGREG';
+				$row['CDTIPCON'] = 3;
+				break;
+			case 'Outros':
+				$row['ASSUNTO'] = 'OUTROS';
+				$row['CDTIPCON'] = 4;
+				break;
+		}
+		$codigos = $this->geraCodigo($row['ASSUNTO'], $row);
+		$row['NRSEQCON'] = $codigos['NRSEQCON'];
+		$row['NRSEQUE']  = $codigos['NRSEQUE'];
+		$row['DTCONTA']  = date("d/m/Y");
+		// Insere no banco de dados e retorna $resulBanco que é um boolean onde diz se a inserção deu certo
+		$resulBanco = $this->insertBanco($row['NRSEQCON'], $row['NOMECLIE'], $row['EMAIL'], $row['ASSUNTO'], $row['TELEFONE'], 
+			$row['MENSAGEM'], $row['NRSEQUE'], $row['DTCONTA'], '', $row['CDTIPCON'], '0001');
+		
+		return $resulBanco;
+	}
+
+
+	//----------------------------------------------------------------------------//
+
+	// Inser no banco os dados do contato
+	protected function insertBanco($NRSEQCON, $NOMECLIE, $EMAIL, $ASSUNTO, $TELEFONE, $MENSAGEM, $NRSEQUE, $DTCONTA,
+		$DTRESPO, $CDTIPCON, $CDOPERA) {
+
+		$link = new mysqli('localhost', 'root', '', 'padaria');
+		$query = "INSERT INTO `contatos`(`NRSEQCON`, `NOMECLIE`, `EMAIL`, `ASSUNTO`, `TELEFONE`, `MENSAGEM`, `NRSEQUE`, `DTCONTA`, `CDTIPCON`, `CDOPERA`) VALUES ('$NRSEQCON', '$NOMECLIE', '$EMAIL', '$ASSUNTO', '$TELEFONE', '$MENSAGEM', '$NRSEQUE', STR_TO_DATE('$DTCONTA', '%d/%m/%Y'), '$CDTIPCON', '$CDOPERA')";
+		$resulBanco = mysqli_query($link, $query);
+		return $resulBanco;
+		
+	}
+
+	// Gera os códigos sequencias para inserção no banco de dados 'contatos' e os grava no banco newCode
+	protected function geraCodigo($primaryKey) {
+		
+	
+		$link = new mysqli('localhost', 'root', '', 'padaria');
+		// Busca todos os codigos sequencias na tabela newCode para aquele tipo de contato
+		$queryTipoContato = "SELECT * FROM newcode WHERE TABLEPK = '$primaryKey' ORDER BY NRSEQUENCIAL ASC";
+		$resultTipoContato = mysqli_query($link, $queryTipoContato); 
+
+		// Busca todos os codigos sequencias na tabela newCode
+		$query = "SELECT * FROM newcode WHERE TABLEPK = 'CONTATO' ORDER BY NRSEQUENCIAL ASC";
+		$resultado = mysqli_query($link, $query);
+
+		// Inicializa variaveis do codigo sequencial maximo
+		$sequencialMax = 0;
+		$sequencialMaxTipoContato = 0;
+
+		// Verifica se existe algum codigo no banco para aquele tipo de Contato
+		if(mysqli_num_rows($resultTipoContato) != 0){
+			// Caso exista descobre qual o ultimo codigo
+			$stop = false; // Variavel que indica a hora de parar
+			// Percorre o vetor do resultado
+			foreach ($resultTipoContato as $key => $value) {
+				// Verifica se o codigo sequencial é maior que o maximo e se não é hora de parar
+				if($value['NRSEQUENCIAL'] > $sequencialMaxTipoContato && $stop === false) {
+					// Verifica se a diferença entre o sequencial atual e o sequencial Maximo é de 1 se for 
+					// torna o sequencial atual como maximo temporariamente 
+					if(intval($value['NRSEQUENCIAL']) - 1 === $sequencialMaxTipoContato) {	
+						$sequencialMaxTipoContato = intval($value['NRSEQUENCIAL']);
+					}
+					// Se a diferença for maior do que um define que é a hora de parar e coloca o atual como
+					// definitivamente mesmo que haja outros elementos no vetor(Isso acontece quando há saltos
+					// de codigo exemplo 01 e depois 03)
+					else if(intval($value['NRSEQUENCIAL']) - $sequencialMaxTipoContato > 1){
+						$stop = true;
+					}
+					else {
+						$sequencialMaxTipoContato = intval($value['NRSEQUENCIAL']) + 1;
+						$stop = true;
+					}
+				}
+			}
+		}
+		else {
+			$sequencialMaxTipoContato = 0;
+		}
+
+		if(mysqli_num_rows($resultado) != 0) {
+			$stop2 = false;
+			foreach ($resultado as $key => $value) {
+				if(intval($value['NRSEQUENCIAL']) > $sequencialMax && $stop2 === false) {
+					if($value['NRSEQUENCIAL'] - 1 === $sequencialMax) {
+						$sequencialMax = intval($value['NRSEQUENCIAL']);
+					}
+					else if(intval($value['NRSEQUENCIAL']) - $sequencialMax > 1){
+						$stop = true;
+					}
+					else {
+						$sequencialMax = intval($value['NRSEQUENCIAL']) + 1;
+						$stop2 = true;
+					}
+				}
+			}
+		}
+		else {
+			$sequencialMax = 0;
+		} 
+
+		
+		$sequencialTipoContato = $sequencialMaxTipoContato + 1; // acrescenta 1 ao maximo
+		$sequencial = $sequencialMax + 1;	// acrescenta 1 ao maximo
+		$sequencialLength = 11 - strlen($sequencial);
+		$sequencialTipoContatoLength = 5 - strlen($sequencialTipoContato); 
+		$sequencial = str_pad($sequencial, $sequencialLength, "0", STR_PAD_LEFT); // formata
+		$sequencialTipoContato = str_pad($sequencialTipoContato, $sequencialTipoContatoLength, "0", STR_PAD_LEFT); // formata
+		$codigos = array("NRSEQCON" => $sequencial, "NRSEQUE" => $sequencialTipoContato);
+		$insert = "INSERT INTO `newcode`(`NRSEQUENCIAL`, `TABLEPK`) VALUES ('$sequencial','CONTATO')"; 
+		mysqli_query($link, $insert);
+		$insertContato = "INSERT INTO `newcode`(`NRSEQUENCIAL`, `TABLEPK`) VALUES ('$sequencialTipoContato','$primaryKey')";
+		mysqli_query($link, $insertContato); 
+
+		return $codigos;
+
+	}
+}
+?>
